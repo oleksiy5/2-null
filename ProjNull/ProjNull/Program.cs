6d7ec5fd583e8e9f802d07033940fcf138459106
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-19 17:26:50 +0200

    s

---
+++ ProjNull/ProjNull/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ProjNull
+{
+    class Program
+    {
+        const string path = @"c:\moj_folder\plik_test.txt";
+
+        static void Main(string[] args)
+        {
+            string txt = null;
+
+            if (File.Exists(path))
+            {
+                txt = File.ReadAllText(path);
+            }
+
+            if (txt == null)
+                Console.WriteLine("Nie udało się przeczytać pliku");
+            else
+                Console.WriteLine(txt);
+
+            Console.ReadKey();
+        }
+    }
+}
